0a7fdd5055118230fce714aca3fbcd16fa6fe419
mikrukovd <z123456689z@gmail.com>
2025-03-13 10:14:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from scripts import get_year, get_drinks_info
+from scripts import get_year, get_sorted_drinks
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-
-template = env.get_template('template.html')
+YEAR_OF_FOUNDATION = 1920
 
 
-render_page = template.render(
-    year=get_year(datetime.datetime.now().year - 1920),
-    drinks=get_drinks_info()
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+
+    template = env.get_template('template.html')
+
+    render_page = template.render(
+        year=get_year(datetime.datetime.now().year - YEAR_OF_FOUNDATION),
+        sorted_drinks=get_sorted_drinks('wine3.xlsx')
+    )
+
+    with open('index.html', 'w', encoding='utf8') as file:
+        file.write(render_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
-with open('index.html', 'w', encoding='utf8') as file:
-    file.write(render_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
